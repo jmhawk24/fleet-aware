d01fcdaf5d2063bb3666684dbd64a2d0323941b7
Jacob Hawk <jhawk@splashfinancial.com>
2020-09-19 21:04:32 -0400

    add button, get dynamic data into Truck List

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import React from 'react';
 import logo from './logo.svg';
 import Checkbox from './Checkbox';
 import TruckPanel from './TruckPanel';
+import MoveButton from './MoveButton';
 import './App.css';
+
+const truckList = [];
+
+const TruckList = (props) => {
+  <div>
+    {truckList.map(truck => <TruckPanel {...truck}/>)}<TruckPanel />
+  </div>
+};
 
 function App() {
   return (
     <div className="App">
-      <TruckPanel />
+      <MoveButton />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/MoveButton.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class MoveButton extends React.Component {
+    render() {
+        return (
+            <div>
+                <button>
+                    Click me.
+                </button>
+            </div>
+        );
+    }
+}
+
+export default MoveButton;

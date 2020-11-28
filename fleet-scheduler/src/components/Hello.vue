db4df09b50326f805a46f73015ed76c2d5d1a09c
Jacob Hawk <jhawk@splashfinancial.com>
2020-11-27 23:33:16 -0500

    locationbox and parent component created

--- fleet-scheduler/src/components/Hello.vue
+++ fleet-scheduler/src/components/Hello.vue
@@ -1,15 +1,37 @@
 <template>
   <div>
       <h1> Hello</h1>
+    <location-box v-for="location in locations" :key="location.id" :trucks="trucks" :name="location.name"></location-box>
   </div>
 </template>
 
 <script>
+import LocationBox from './LocationBox'
+
+var truckData = [
+    {id: 1, name: 'MCT1', location: 1},
+    {id: 2, name: 'MCT2', location: 1}
+];
+
+var locationData = [
+    {id: 1, name: 'Theatre'},
+    {id: 2, name: 'Second Lot'},
+    {id: 3, name: 'Bob\'s Repair Dungeon'}
+]
+
 export default {
-    
+  components: { LocationBox },
+  computed: {
+      trucks: function() {
+          return truckData
+      },
+      locations: function() {
+          return locationData
+      }
+  },
 }
 </script>
 
 <style>
 
-</style>
+</style>LocationBox
---
+++ fleet-scheduler/src/components/LocationBox.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h1>{{name}}</h1>
+        <div v-for="truck in currentTrucks" :key="truck.id">
+            {{truck.name}}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['trucks', 'name'],
+    computed: {
+        currentTrucks: function() {
+            return this.trucks
+        }
+    },
+    data: function() {
+        return {
+            currentTruckCount: 0
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
